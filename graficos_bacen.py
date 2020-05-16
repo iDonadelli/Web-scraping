d3297425e108f49c4b518def746a63d949f6b395
iDonadelli <56171465+iDonadelli@users.noreply.github.com>
2020-05-16 17:57:06 -0300

    Add files via upload

---
+++ graficos_bacen.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def grafico_bacen (code, type, titulo):
+    url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json'.format(code)
+    dado = pd.read_json(url)
+
+    x = dado['data'][-25:]
+    y = dado['valor'][-25:]
+    plt.style.use('ggplot')
+
+    if type == 'linear':
+        plt.plot(x, y, color='teal', linewidth=2)
+    elif type == 'barras':
+        plt.bar(x, y, color='teal')
+    else: plt.scatter(x, y, color='teal', edgecolors='black')
+     
+    plt.title(titulo)
+    plt.xticks(x, x, rotation='vertical')
+    plt.tick_params(labelsize=10)
+    plt.show()
+
+
+grafico_bacen (7326, 'DISPERSAO', 'PIB - Taxa variação real')
+
